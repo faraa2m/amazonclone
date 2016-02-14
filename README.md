de005261019c930da610ccd53520faa5c92f928b
mohdfaraaz1 <fmohamme@syr.edu>
2016-02-14 00:40:44 -0500

    Adding mongoose bcrypt-nodejs

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ Dependencies Required:
 
 	install other dependecies using npm init
 
-	1) Express
-	2
+	1) Express -- for routing
+	2) Morgan -- login Request Library
+	3) Mongoose -- to connect to and use mongodb (mongolab in this case)
+	4) bcrypt-nodejs -- Native JS implementation of BCrypt for Node.
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var express = require('express');
+var express 	= require('express');
+var morgan 		= require('morgan');
+var mongoose 	= require('mongoose'); 
+var app 		= express();
 
-var app = express();
+//Middlewares
+app.use(morgan('dev'));
 
 app.get("/",function(req,res){
 	res.send("HomePage");
